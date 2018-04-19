3e3a43b587aef788009153f0860385e9da5eef2b
sohom <sohom.roy@niser.ac.in>
2018-04-19 08:15:41 +0000

    Jacobi method

--- Linear Equations/jacobi.m
+++ Linear Equations/jacobi.m
@@ -1,16 +1,31 @@
 #JACOBI METHOD
 
-A=load('mat.txt')
+A=load('jacobimat.txt')
 
 n=rows(A)
 m=columns(A)
 
 b=A(:,m);
 A=A(:,[1:n]);
-x=zeros(1,n);
+
+N=input("Enter number of iterations: ");
+
+x=zeros(N,n);
 
 for i=1:n
   str=sprintf("Enter initial guess for x%d: ",i);
-  x(i)=input(str);
+  x(1,i)=input(str);
+endfor
+
+for m=1:N-1
+  for i=1:n
+    S=0;
+    for j=1:n
+      if(i!=j)
+        S+=A(i,j)*x(m,j);
+      endif
+    endfor
+    x(m+1,i)=(b(i)-S)/A(i,i);
+  endfor
 endfor
 
